ef9490883741d9a72693ad5d1cd4ce56493538a0
LehighCSE262 <cmc321@lehigh.edu>
2020-12-24 16:43:34 -0500

    Typescript migration first attempt

--- README.md
+++
@@ -1,7 +0,0 @@
-## Usage
- 
-```npm install``` install dependencies
-
-```npm run watch``` watch client and server at localhost:3000
-
-```npm run build-client``` build and minify client js for production
---
+++ server/server.js
@@ -0,0 +1,25 @@
+var port = process.env.PORT || 8080;
+var http = require("http");
+var express = require("express");
+var path = require("path");
+var bodyParser = require("body-parser");
+var routes = require("./modules/routes.js");
+var api = require("./modules/api.js");
+
+var app = express();
+
+app.set("view engine", "ejs");
+//app.use(compression());
+// app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, "../client/public")));
+
+app.use(api);
+app.use(routes); // routes should be below api, to catch undeclared routes and render page 404
+
+var server = http.createServer(app);
+server.listen(port);
+console.log("Server is running at localhost:" + port);
