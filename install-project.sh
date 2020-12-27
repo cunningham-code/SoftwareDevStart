6bd5d8e5d943b60f813b1278b160aff4a434f13c
LehighCSE262 <cmc321@lehigh.edu>
2020-12-26 23:20:01 -0500

    Separated client and server modules

---
+++ install-project.sh
@@ -0,0 +1,30 @@
+###
+#   This script should only be used once when project is first cloned
+#   NOTE: Will not work if dependencies are added or versions are updated
+#         For these changes, you must manually run `npm install` in the local
+#         directory being updated
+###
+
+### Client Dependencies ###
+
+cd ./client
+# Checks to make sure the node_modules folder exists
+if [ ! -d ./node_modules ]
+then
+    npm install
+else
+    echo "Client dependencies already installed!"    
+fi
+
+### Server Dependencies ###
+cd ../server
+# Checks to make sure the node_modules folder exists
+if [ ! -d ./node_modules ]
+then
+    npm install
+else
+    echo "Server dependencies already installed!"    
+fi
+
+# return to root level
+cd ..
